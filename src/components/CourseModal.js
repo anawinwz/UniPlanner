b52db78e439704b333a396aad3059e898d83c761
Anawin W <imonmac@hotmail.com>
2019-12-04 23:02:27 +0700

    Add simple course modal

---
+++ src/components/CourseModal.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+
+import { Modal } from 'antd';
+import { courseContext } from '../contexts';
+
+export default (props) => {
+  const { courses, updateCourse } = useContext(courseContext);
+  
+  const mode = (props.key) ? 'edit' : 'add';
+  if (mode === 'edit') {
+    const targetIdx = courses.findIndex(course => course.key === props.key);
+    const targetInfo = courses[targetIdx];
+  }
+
+  return <Modal
+    title={mode === 'edit' ? `แก้ไขข้อมูลรายวิชา` : `เพิ่มข้อมูลรายวิชา`}
+    visible={props.visible}
+    onOk={props.handleOk}
+    onCancel={props.handleCancel}
+  >
+    <p>Some contents...</p>
+  </Modal>
+};
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,12 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { Tree, Button, Icon } from 'antd';
 import { planContext, courseContext } from '../contexts';
 
+import CourseModal from './CourseModal';
+
 const { TreeNode } = Tree;
 
 export default (props) => {
+  const [ modal, setModal ] = useState({ visible: false, key: null });
+
+  
   const { courses } = useContext(courseContext);
+  function showAddModal() { showEditModal(); }
+  function showEditModal(key) {
+    setModal({visible: true, key: (key) ? key : null});
+  }
+  function handleOk() {
+    setModal({visible: false});
+  }
+  function handleCancel() {
+    setModal({visible: false});
+  }
+
   const { selected, plans, updatePlan } = useContext(planContext);
 
   const selectedIdx = plans.findIndex(plan => plan.key === selected);
@@ -24,7 +40,7 @@ export default (props) => {
 
   return (
     <div>
-      <Button type="primary"><Icon type="plus" /> เพิ่ม</Button>
+      <Button type="primary" onClick={showAddModal}><Icon type="plus" /> เพิ่ม</Button>
       <Tree
         selectable={false}
         checkable
@@ -39,6 +55,7 @@ export default (props) => {
           </TreeNode>
         )}
       </Tree>
+      <CourseModal visible={modal.visible} handleOk={handleOk} handleCancel={handleCancel} />
     </div>
   );
   
