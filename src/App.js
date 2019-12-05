463b9ace1e12083e326f739f68be54669ebb9d2d
Anawin W <imonmac@hotmail.com>
2019-12-05 12:00:03 +0700

    Add localStorage saving

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
@@ -9,22 +9,32 @@ import './App.css'
 import { planContext, courseContext } from './contexts';
 
 function App() {
+  const initialPlans = [{
+    name: 'แผนเริ่มต้น',
+    courses: ["01204322_1","01205386_11"],
+    key: 'default'
+  }]
   const [plan, setPlan] = useState({
     selected: 'default',
-    plans: [{
-      name: 'แผนเริ่มต้น',
-      courses: ["01204322_1","01205386_11"],
-      key: 'default'
-    }],
+    plans: JSON.parse(localStorage.getItem('plans')) || initialPlans,
     updatePlan: update => setPlan(plan => ({...plan, ...update}))
   })
+  const initialCourses = [
+    {"key":"01204322","credits":3,"name":"Embedded","required":true,"sections":[{"name":1,"lects":[{"dow":["M"],"start":"13:00","end":"16:00"}],"key":1}]},
+    {"key":"01205386","credits":1,"name":"Elect.Lab.","required":true,"sections":[{"name":"11","lects":[{"dow":["Th"],"start":"13:00","end":"16:00"}],"key":"11"}]}
+  ];
   const [course, setCourse] = useState({
-    courses: [
-      {"key":"01204322","credits":3,"name":"Embedded","required":true,"sections":[{"name":1,"lects":[{"dow":["M"],"start":"13:00","end":"16:00"}],"key":1}]},
-      {"key":"01205386","credits":1,"name":"Elect.Lab.","required":true,"sections":[{"name":"11","lects":[{"dow":["Th"],"start":"13:00","end":"16:00"}],"key":"11"}]}
-    ],
+    courses: JSON.parse(localStorage.getItem('courses')) || initialCourses,
     updateCourse: update => setCourse(course => ({...course, ...update}))
   })
+
+  useEffect(() => {
+      localStorage.setItem('plans', JSON.stringify(plan.plans));
+  }, [plan]);
+  useEffect(() => {
+    localStorage.setItem('courses', JSON.stringify(course.courses));
+  }, [course]);
+
   return (
     <planContext.Provider value={plan}>
       <courseContext.Provider value={course}>
